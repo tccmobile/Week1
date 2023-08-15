e764a16491dc0bf18604ebbd2efd3588ecfbfc4c
William Smith <william.smith@tulsacc.edu>
2023-08-15 12:14:16 -0500

    updates

--- main.cpp
+++ main.cpp
@@ -5,4 +5,4 @@ int main() {
 
     cout << "Hello, World!" << endl;
     return 0;
-}`
+}
---
+++ warmup.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int userNum;
+
+   cout << "Enter integer:" << endl;
+   cin  >> userNum;
+   
+   /* Type your code here */
+
+   return 0;
+}
